e93fc20f01b940bf85e93eeb6c1b581dd4cb4f1e
rokkincat <josh@rokkincat.com>
2014-12-05 16:46:07 -0600

    Updated podspec

--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -18,3 +18,9 @@
 #define COCOAPODS_VERSION_MINOR_Phoenix_ObjC 1
 #define COCOAPODS_VERSION_PATCH_Phoenix_ObjC 0
 
+// SocketRocket
+#define COCOAPODS_POD_AVAILABLE_SocketRocket
+#define COCOAPODS_VERSION_MAJOR_SocketRocket 0
+#define COCOAPODS_VERSION_MINOR_SocketRocket 3
+#define COCOAPODS_VERSION_PATCH_SocketRocket 1
+
